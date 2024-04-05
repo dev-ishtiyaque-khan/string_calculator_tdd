bb548c4fa6a4b56fad2f8321fe2d2cba5d58f337
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 21:41:17 +0530

    Add failing test to handle new lines between numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,7 +5,7 @@ describe StringCalculator do
 
   describe '#add' do
     shared_examples 'calculate sum of input' do |numbers, sum|
-      it "returns #{sum} for input #{numbers}" do
+      it "returns #{sum} for input #{numbers.inspect}" do
         expect(calculator.add(numbers)).to eq(sum)
       end
     end
@@ -14,5 +14,6 @@ describe StringCalculator do
     include_examples 'calculate sum of input', '1', 1
     include_examples 'calculate sum of input', '1,5', 6
     include_examples 'calculate sum of input', '1,3,6', 10
+    include_examples 'calculate sum of input', "1\n2,3", 6
   end
 end
