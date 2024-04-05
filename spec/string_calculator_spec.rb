30c499d8dd7e692249d7e29ac48ec0224f0d008e
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 21:06:41 +0530

    Add failing test for a single number input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,4 +8,8 @@ describe StringCalculator do
       expect(calculator.add('')).to eq(0)
     end
   end
+
+  it 'returns number for a single number' do
+    expect(calculator.add('1')).to eq(1)
+  end
 end
