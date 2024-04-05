7854242706a3ddb30ff7626fe3e50efe28be6d87
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 21:03:28 +0530

    Add failing test for an empty string input

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+require './lib/string_calculator'
+
+describe StringCalculator do
+  subject(:calculator) { described_class.new }
+
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(calculator.add('')).to eq(0)
+    end
+  end
+end
