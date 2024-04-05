6a6ed6a0893c9351b54bbeef389711a6276fdfff
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 21:47:59 +0530

    Changes to handle new lines between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(',').reduce(0) { |sum, number| sum + number.to_i }
+    numbers.split(/[,\n]/).reduce(0) { |sum, number| sum + number.to_i }
   end
 end
