26cb0019d4efa6232e7e173dc47250a684c95812
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 21:25:30 +0530

    Implement basic functionality to return sum for comma-separated numbers as string input

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,11 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.to_i
+    if numbers.include?(',')
+      first_number, second_number = numbers.split(',')
+      first_number.to_i + second_number.to_i
+    else
+      numbers.to_i
+    end
   end
 end
