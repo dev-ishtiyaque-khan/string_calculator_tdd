c9817184a664afc3b74cafbd3d24ee72ba9114bd
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 21:36:53 +0530

    Changes to handle any unknown amount of numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,11 +2,6 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    if numbers.include?(',')
-      first_number, second_number = numbers.split(',')
-      first_number.to_i + second_number.to_i
-    else
-      numbers.to_i
-    end
+    numbers.split(',').map(&:to_i).sum
   end
 end
