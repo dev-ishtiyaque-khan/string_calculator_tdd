5301921a910044305b328f389c97779a57ccce82
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 23:12:38 +0530

    Add failing test to verify sum for numbers with custom delimitor

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,11 +17,17 @@ describe StringCalculator do
     end
 
     context 'with valid input' do
-      include_examples 'calculate sum of input', '', 0
-      include_examples 'calculate sum of input', '1', 1
-      include_examples 'calculate sum of input', '1,5', 6
-      include_examples 'calculate sum of input', '1,3,6', 10
-      include_examples 'calculate sum of input', "1\n2,3", 6
+      context 'with default delimiter' do
+        include_examples 'calculate sum of input', '', 0
+        include_examples 'calculate sum of input', '1', 1
+        include_examples 'calculate sum of input', '1,5', 6
+        include_examples 'calculate sum of input', '1,3,6', 10
+        include_examples 'calculate sum of input', "1\n2,3", 6
+      end
+
+      context 'with custom delimiter' do
+        include_examples 'calculate sum of input', "//;\n1;2", 3
+      end
     end
 
     context 'with invalid input' do
