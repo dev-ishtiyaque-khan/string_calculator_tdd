9e76af84acc882ab87ca9d15a2860c0b057d2a0d
ishtiyaque.khan <ishtiyaquekhan141@gmail.com>
2024-04-05 23:30:42 +0530

    Refactor code with encapsulating the logic of delimeter and number extraction

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,19 +1,14 @@
 class StringCalculator
   class InvalidInputError < StandardError; end
 
+  DEFAULT_DELIMETER = Regexp.new('[,\n]')
+
   def add(numbers)
     return 0 if numbers.empty?
 
     validate_input!(numbers)
 
-    delimiter, numbers = if numbers.start_with?("//")
-      delimiter_line, numbers = numbers.split("\n", 2)
-      delimiter = delimiter_line[2..-1]
-      [Regexp.escape(delimiter), numbers]
-    else
-      [Regexp.new('[,\n]'), numbers]
-    end
-
+    delimiter, numbers = extract_delimiter_and_numbers(numbers)
     numbers.split(/#{delimiter}/).reduce(0) { |sum, number| sum + number.to_i }
   end
 
@@ -21,5 +16,17 @@ class StringCalculator
 
   def validate_input!(numbers)
     raise InvalidInputError, numbers if numbers.end_with?("\n") || numbers.end_with?(',')
+  end
+
+  def extract_delimiter_and_numbers(numbers)
+    delimiter = DEFAULT_DELIMETER
+    delimiter_match = numbers.match(/\/\/(.*)\n(.*)/)
+
+    if delimiter_match
+      delimiter = Regexp.escape(delimiter_match[1])
+      numbers = delimiter_match[2]
+    end
+
+    [delimiter, numbers]
   end
 end
